f1f47769ad32a8ced9ee2177dab59429c9c850fb
daaviidmartinez <44229292+daaviidmartinez@users.noreply.github.com>
2019-09-19 11:49:54 +0200

    Update class.php

--- class.php
+++ class.php
@@ -1,6 +1,6 @@
 <?php
 
-    class cuentaBancaria{
+    class cuentaBancaria{ //Creamos clase 
         public $titular;
         public $cantidad;
 
@@ -10,21 +10,26 @@
 
         }
         public function ingreso($ingreso=0){
-            $this->cantidad+=$ingreso;
-        }
-        public function retiro($retirada=0){
-            $this->cantidad-=$ingreso;
-        }
-        public function notificar($mensaje){
-            if ($this->cantidad = 0){
-                echo ('Tienes 0€ en el banco');
+            $this->cantidad+=$ingreso; //Suma la cantidad + el ingreso
+            if ($ingreso > 0) {
+                $this->notificar('Has ingresado '. $ingreso.' €'.'<br>Tienes en total '.$this->cantidad.' €');
             }
         }
-
+        public function retiro($retiro=0){
+            $this->cantidad-=$retiro; //Resta la cantidad - el retiro
+            if ($retiro > 0) {
+                $this->notificar('Has retirado '. $retiro.' €'.'<br> Tienes en total '. $this->cantidad.' €');
+            } elseif ($cantidad < $retiro) {
+                $this->notificar('No tienes tanto dinero perro');
+            }   
+        }
+        public function notificar($mensaje){    
+            echo ($mensaje); 
+        }
     }
     
-    $cuenta1 = new CuentaBancaria ('Jose Antonio',2000);
-
-    $cuenta1->ingreso(200);echo '<br>';
-
+    $cuenta1 = new CuentaBancaria ('Jose Antonio',400); //Parametros que se pondran en titular y cantidad
+    $cuenta1->ingreso();echo '<br>'; //Valores que queramos ingresar
+    $cuenta1->retiro(500);echo '<br>'; //Valores que queremos retirar
 ?>
+
